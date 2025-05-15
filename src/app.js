25cc87bc39c7bc5f970e3b1e568b06ff12ea72cf
José Rey Arijón <144047251+joshMonarch@users.noreply.github.com>
2025-05-15 13:32:03 +0000

    Firt commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const p = document.createElement("p")
+  p.id = "excuse"
+  console.log(p)
+  const wrapper = document.getElementsByClassName("wrapper")[0]
+  const button = document.querySelector("button")
+
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+
+
+
+  function getRandomElement(list){
+    return list[Math.floor(Math.random() * list.length)]
+  }
+
+  button.onclick = function generateExcuse(){
+    wrapper.appendChild(p)
+    let myExcuse = ""
+
+    for (let words of [who, action, what, when]){
+      myExcuse += (" " + getRandomElement(words))
+    }
+    p.innerHTML = myExcuse.trimStart() + ".";
+  }
+
 };
